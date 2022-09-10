4c922eb93686865392f013225fd18ef88ca33737
rajeev-t <thimmareddyrajeev@gmail.com>
2022-09-09 18:55:06 -0700

    Uploaded data, pseudocoded game plan

---
+++ PyPoll.py
@@ -0,0 +1,6 @@
+#Data to retrieve 
+# 1. Total # of votes cast
+# 2. Complete list of candidates who recieved votes
+# 3. Total # of votes each candidate won
+# 4. % of votes each candidate won
+# 5. Winner of election based on popular vote
